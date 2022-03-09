42a3a5f93f2f31686102b54f4f94b54b0d0afb1f
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2022-03-09 23:57:06 +0300

    fix sidenav styles

--- src/app/main/views/sidenav/sidenav.view.ts
+++ src/app/main/views/sidenav/sidenav.view.ts
@@ -4,4 +4,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './sidenav.view.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidenavView {}
+export class SidenavView {
+  public links = Array.from(
+    { length: 40 },
+    () => Math.floor(Math.random() * 40),
+  );
+}
