905bdb931e991617219f344ae2ac2ce4725d2987
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2022-03-20 16:54:58 +0300

    add zone rxjs operators;

--- projects/core/rxjs/index.ts
+++ projects/core/rxjs/index.ts
@@ -1,2 +1,5 @@
 export { autocomplete } from './autocomplete.rxjs';
 export { watch } from './watch.rxjs';
+export { zoneIn } from './zone-in.rxjs';
+export { zoneOut } from './zone-out.rxjs';
+export { zoneOptimized } from './zone-optimized.rxjs';
---
+++ projects/core/rxjs/zone-out.rxjs.ts
@@ -0,0 +1,12 @@
+import { NgZone } from '@angular/core';
+
+import { MonoTypeOperatorFunction, Observable } from 'rxjs';
+
+export function zoneOut<T>(
+  ngZone: NgZone
+): MonoTypeOperatorFunction<T> {
+  return source =>
+    new Observable(subscriber =>
+      ngZone.runOutsideAngular(() => source.subscribe(subscriber)),
+    );
+}
