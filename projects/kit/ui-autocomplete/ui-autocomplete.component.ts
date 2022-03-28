631b7fefad23ee824d326677271e825b30ddc269
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2022-03-28 17:05:28 +0300

    add accessibility to ui-autocomplete add trim to innerText for icons

--- projects/kit/ui-autocomplete/ui-autocomplete.component.ts
+++ projects/kit/ui-autocomplete/ui-autocomplete.component.ts
@@ -1,7 +1,9 @@
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   ContentChildren,
+  Inject,
   Input,
   OnChanges,
   QueryList,
@@ -9,39 +11,70 @@ import {
   TemplateRef,
   ViewChild
 } from '@angular/core';
+import { DOCUMENT, NgTemplateOutlet } from '@angular/common';
+import { ActiveDescendantKeyManager, Highlightable } from '@angular/cdk/a11y';
+import { DOWN_ARROW, ENTER, UP_ARROW } from '@angular/cdk/keycodes';
 
-import { identity, merge, Observable, startWith, Subject, switchMap } from 'rxjs';
+import { filter, fromEvent, identity, merge, Observable, startWith, Subject, switchMap, takeUntil } from 'rxjs';
 
 import { TODO_ANY } from '@ui-components/core/types';
 import { UiOptionComponent } from '@ui-components/kit/ui-option';
+import { UiDestroyedService } from '@ui-components/core/services';
 
 @Component({
   selector: 'ui-autocomplete',
   exportAs: 'uiAutocomplete',
   templateUrl: './ui-autocomplete.component.html',
   styleUrls: ['./ui-autocomplete.component.scss'],
+  providers: [ UiDestroyedService ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UiAutocompleteComponent
-  implements OnChanges {
+  implements OnChanges, AfterContentInit {
 
   @Input()
   public displayWith: (value: TODO_ANY) => string = identity;
 
   @ViewChild('root', { read: TemplateRef })
-  public readonly templateRef: TemplateRef<any> | null = null;
+  public readonly templateRef: TemplateRef<NgTemplateOutlet> | null = null;
 
   @ContentChildren(UiOptionComponent)
-  public options: QueryList<UiOptionComponent> = new QueryList<UiOptionComponent>();
+  public options: QueryList<Highlightable & UiOptionComponent>
+    = new QueryList<Highlightable & UiOptionComponent>();
 
   public readonly displayWith$ = new Subject<void>();
 
+  /** Keyboard accessibility **/
+  private _keyManager: ActiveDescendantKeyManager<UiOptionComponent>
+    = new ActiveDescendantKeyManager<UiOptionComponent>([]);
+
+  constructor(
+    @Inject(DOCUMENT)
+    private readonly _document: Document,
+    @Inject(UiDestroyedService)
+    private readonly _destroyed$: UiDestroyedService,
+  ) {
+    this._subKeydown();
+  }
+
   public ngOnChanges(changes: SimpleChanges): void {
+    /** Need to update displayWith value in the UiAutocompleteDirective **/
     if (changes['displayWith'].firstChange) {
       this.displayWith$.next(changes['displayWith'].currentValue);
     }
   }
 
+  public ngAfterContentInit(): void {
+    this._keyManager = new ActiveDescendantKeyManager<UiOptionComponent>(
+      this.options,
+    )
+      .withWrap()
+      .withTypeAhead();
+  }
+
+  /**
+   * Select option via Click
+   * */
   public getOptionSelected$(): Observable<TODO_ANY> {
     return this.options.changes
       .pipe(
@@ -52,6 +85,30 @@ export class UiAutocompleteComponent
           return merge(...click$);
         })
       );
+  }
+
+  /**
+   * If the arrow_up or arrow_down buttons were pressed then apply active class to option
+   * if the enter button was pressed then emit OnClick() event of UiOptionComponent.
+   **/
+  private _subKeydown(): void {
+    fromEvent<KeyboardEvent>(this._document, 'keydown')
+      .pipe(
+        filter(({ target }: KeyboardEvent) => {
+          return target instanceof HTMLInputElement || target instanceof UiOptionComponent;
+        }),
+        filter(({ keyCode }: KeyboardEvent) => {
+          return keyCode === UP_ARROW || keyCode === DOWN_ARROW || keyCode === ENTER;
+        }),
+        takeUntil(this._destroyed$),
+      )
+      .subscribe((event: KeyboardEvent) => {
+        if (event.keyCode === ENTER) {
+          this._keyManager.activeItem?.onClick();
+        } else {
+          this._keyManager.onKeydown(event);
+        }
+      });
   }
 
 }
