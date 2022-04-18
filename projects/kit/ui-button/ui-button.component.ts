1667e5b1925f2848ad1871c73ada85428c4b5bef
Nikita Kovalev <nikita.kovalev.work@gmail.com>
2022-04-18 11:33:36 +0300

    fix overlay bugs

--- projects/kit/ui-button/ui-button.component.ts
+++ projects/kit/ui-button/ui-button.component.ts
@@ -1,11 +1,12 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
 
 import { Palette } from '@ui-components/core/types';
 
 @Component({
   selector: 'button[uiButton], button[uiRaisedButton], button[uiIconButton]',
   template: `<ng-content></ng-content>`,
-  styleUrls: ['./ui-button.component.scss']
+  styleUrls: ['./ui-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UiButtonComponent {
 
